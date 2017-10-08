717dd0b5b209501be398d6aeed4729c9265374ce
manish-singhal <manish.singhal@tamu.edu>
2017-10-08 09:37:03 -0500

    Add new files and folders
    Add new files and folders

--- PhageBank/urls.py
+++ PhageBank/urls.py
@@ -1,27 +1,18 @@
-"""
-Definition of urls for BioBank.
-"""
-
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+from PhageBank.core import views as core_views
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', BioBank.views.home, name='home'),
-    # url(r'^BioBank/', include('BioBank.BioBank.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
