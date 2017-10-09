0f44aad301772e21dabc29f08045f30bd5277940
Apoorv Kumar <apoorvkumar91@tamu.edu>
2017-10-09 16:34:16 -0500

    a

--- PhageBank/settings.py
+++ PhageBank/settings.py
@@ -99,17 +99,17 @@ ALLOWED_HOSTS = ['*']
 
 # end - AK - more additions at the end of the file
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.config()
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+#DATABASES = {
+#    'default': dj_database_url.config()
+#    
+#}
 
 #DATABASES['default'] =  dj_database_url.config()
 #
