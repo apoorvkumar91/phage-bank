de4f4ef3db4f3b229ddc1d141cbcdd881077ff5b
Apoorv Kumar <apoorvkumar91@tamu.edu>
2017-10-09 14:19:35 -0500

    a

---
+++ contact/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+#def index(request):
+#    return render(request, 'contact/contact.html');
+#    #return HttpResponse("<h2> Hi </h2>")
+#    
+def index(request):
+    return render(request, 'contact/contact.html', {'content':['In case of any questions / suggestions, email me at:','apoorvkumar91@tamu.edu']});
