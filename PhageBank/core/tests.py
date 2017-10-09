7b64aeaaea304a9374fb0607ec49d03465bf9c9a
srishti agarwal <agar1605@tamu.edu>
2017-10-08 21:10:21 -0500

    Adding simple test case to check login

--- PhageBank/core/tests.py
+++ PhageBank/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
 
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/login/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
 # Create your tests here.
