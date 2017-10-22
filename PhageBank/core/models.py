d4cafcea53b37480296385594eacd008b3af7d81
srishti agarwal <agar1605@tamu.edu>
2017-10-22 01:24:20 -0500

    Phage_Data_Table

--- PhageBank/core/models.py
+++ PhageBank/core/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class PhageData(models.Model):
+    phage_name = models.CharField(max_length=30)
+    phage_host_id = models.IntegerField()
+    phage_CPT_id = models.CharField(max_length=100)
+    phage_isolator_loc = models.CharField(max_length=5000)
+    phage_experimenter_id = models.IntegerField()
+    phage_isolator_id = models.IntegerField()
