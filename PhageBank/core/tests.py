d7f3807c83bbee908dc3ee6e7edfed2f2c14c384
Apoorv Kumar <apoorvkumar91@tamu.edu>
2017-10-22 19:52:17 -0500

    Fixed the test cases.

--- PhageBank/core/tests.py
+++ PhageBank/core/tests.py
@@ -3,11 +3,16 @@ import unittest
 from django.test import Client
 from django.contrib.auth.models import User
 
+from faker import Faker
+fake=Faker()
+
+username=fake.word()
+
 class SimpleTest(unittest.TestCase):
-    user = User.objects.create(username='testuser', password='pass@123', email='testuser@test.com')
+    user = User.objects.create(username=username, password='pass@123', email='testuser@test.com')
     user.save()
     valid_credentials = {
-        'username': 'testuser',
+        'username': username,
         'password': 'pass@123'}
     invalid_credentials = {
         'username': 'abcde',
@@ -17,7 +22,7 @@ class SimpleTest(unittest.TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
-
+        
     def test_details(self):
         # Issue a GET request.
         response = self.client.get('/login/')
@@ -33,6 +38,7 @@ class SimpleTest(unittest.TestCase):
     def test_user_doesnt_exist(self):
         # Check that login is unsuccessful with invalid user
         response = self.client.post('/login/', self.invalid_credentials, follow=True)
-        index = response.content.find("Your username and password didn't match. Please try again.")
+        index = response.content.find(b"Your username and password didn\'t match. Please try again.")
+        
         self.assertNotEqual(index,-1)
 # Create your tests here.
