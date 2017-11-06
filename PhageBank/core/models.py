53828e340c0d9d15530e654b2899be67956d3711
manish-singhal <manish.singhal@tamu.edu>
2017-11-05 23:31:16 -0600

    1) CSV file read based on column headers 2) entry of phage submission by user and date3) Validation of the type of file(only csv supported)
    1) CSV file read based on column headers
    2) entry of phage submission by user and date
    3) Validation of the type of file(only csv supported)

--- PhageBank/core/models.py
+++ PhageBank/core/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
-
+from datetime import datetime
 
 class PhageData(models.Model):
     phage_name = models.CharField(max_length=30, default='none')
@@ -11,3 +10,5 @@ class PhageData(models.Model):
     phage_CPT_id = models.CharField(max_length=100, default='none')
     phage_isolator_loc = models.CharField(max_length=5000, default='none')
     phage_all_links = models.CharField(max_length=5000, default='none')
+    phage_submitted_user = models.CharField(max_length=5000, default='none')
+    phage_submitted_date = models.DateTimeField(default=datetime.now, blank=True)
--- PhageBank/core/views.py
+++ PhageBank/core/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 import csv
-import io
+from io import StringIO
+from io import TextIOWrapper
 from PhageBank.core.forms import SignUpForm, AddPhageForm, UploadFileForm
 from PhageBank.core.models import PhageData
-
+from csvvalidator import *
+import datetime
+import sqlite3
+import pandas as pd
 
 @login_required
 def home(request):
@@ -35,32 +40,53 @@ def addphage(request):
         phageform = AddPhageForm()
         return render(request, 'addphage.html', {'form': phageform})
 
-def handle_uploaded_file(uploadedfile):
-    csv_file = uploadedfile
-    decoded_file = csv_file.read().decode('utf-8')
-    io_string = io.StringIO(decoded_file)
-    for line in csv.reader(io_string, delimiter=','):
-        PhageData.objects.create(phage_name=line[0],phage_host_name=line[1],phage_isolator_name=line[2],
-                                 phage_experimenter_name=line[3],phage_CPT_id=line[4],phage_isolator_loc=line[5])
-        print(line)
-
 def viewphages(request):
     query_results = PhageData.objects.all()
     return render(request, 'viewphages.html', {'query_results': query_results})
-
 
 def viewPhage(request):
     phageName = request.GET.get('name')
     phage = PhageData.objects.get(phage_name=phageName)
     return render(request, 'viewPhage.html', {'item': phage})
 
+def populate(reader, request):
+    fields = reader.fieldnames
+    for row in reader:
+        flag = 0
+        obj = PhageData.objects.create()
+        if 'phage_name' in fields:
+            obj.phage_name = row['phage_name']
+            flag = 1
+        if 'phage_host_name' in fields:
+            obj.phage_host_name = row['phage_host_name']
+            flag = 1
+        if 'phage_isolator_name' in fields:
+            obj.phage_isolator_name = row['phage_isolator_name']
+            flag = 1
+        if 'phage_experimenter_name' in fields:
+            obj.phage_experimenter_name = row['phage_experimenter_name']
+            flag = 1
+        if 'phage_CPT_id' in fields:
+            obj.phage_CPT_id = row['phage_CPT_id']
+            flag = 1
+        if 'phage_isolator_loc' in fields:
+            flag = 1
+            obj.phage_isolator_loc = row['phage_isolator_loc']
+        obj.phage_submitted_user = request.user.username
+
+        if flag == 0:
+            obj.delete()
+        else:
+            obj.save()
 
 
 def model_form_upload(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            paramFile = TextIOWrapper(request.FILES['file'].file, encoding=request.encoding)
+            reader = csv.DictReader(paramFile,delimiter=';',skipinitialspace=True,)
+            populate(reader, request)
             return redirect('home')
     else:
         form = UploadFileForm()
