e807e0be7dda562d8ff828e69a8b1997befae446
srishti agarwal <agar1605@tamu.edu>
2017-11-22 18:16:24 -0600

    minor changes

--- PhageBank/settings.py
+++ PhageBank/settings.py
@@ -104,7 +104,6 @@ ALLOWED_HOSTS = ['*']
 #CSRF_COOKIE_SECURE = True
 #SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # end - AK - more additions at the end of the file
 
 DATABASES = {
