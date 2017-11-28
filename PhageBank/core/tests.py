89fdbb3663a378004c7d3bde1f2edb572327fefa
manish-singhal <manish.singhal@tamu.edu>
2017-11-28 00:49:15 -0600

    tests
    tests

--- PhageBank/core/tests.py
+++ PhageBank/core/tests.py
@@ -1,44 +1,95 @@
-from django.test import TestCase
-import unittest
-from django.test import Client
-from django.contrib.auth.models import User
-
-from faker import Faker
-fake=Faker()
-
-username=fake.word()
-
-class SimpleTest(unittest.TestCase):
-    user = User.objects.create(username=username, password='pass@123', email='testuser@test.com')
-    user.save()
-    valid_credentials = {
-        'username': username,
-        'password': 'pass@123'}
-    invalid_credentials = {
-        'username': 'abcde',
-        'password': '12345'}
+class URLGETTest(unittest.TestCase):
     client = Client()
 
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
-        
-    def test_details(self):
-        # Issue a GET request.
-        response = self.client.get('/login/')
 
-        # Check that the response is 200 OK.
+    def test_home_url(self):
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_user_exist(self):
-        # Check that login is successful with valid user
-        response = self.client.post('/login/', self.valid_credentials, follow=True)
-        self.assertEqual(response.status_code,200)
+    def test_contact_url(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_user_doesnt_exist(self):
-        # Check that login is unsuccessful with invalid user
-        response = self.client.post('/login/', self.invalid_credentials, follow=True)
-        index = response.content.find(b"Your username and password didn\'t match. Please try again.")
-        
-        self.assertNotEqual(index,-1)
-# Create your tests here.
+    def test_view_phages_url(self):
+        response = self.client.get('/view_phages/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_phage_url(self):
+        response = self.client.get('/search_phage/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_uploads_url(self):
+        response = self.client.get('/uploads/form/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_details_url(self):
+        response = self.client.get('/edit_details/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_change_password_url(self):
+        response = self.client.get('/change_password/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_logout_url(self):
+        response = self.client.get('/logout/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_url(self):
+        response = self.client.get('/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mylogin_url(self):
+        response = self.client.get('/mylogin/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_url(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_addphage_url(self):
+        response = self.client.get('/add_phage/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_deleteall_url(self):
+        response = self.client.get('/delete_all/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_url(self):
+        response = self.client.get('/delete/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_my_phages_url(self):
+        response = self.client.get('/my_phages/')
+        self.assertEqual(response.status_code, 200)
+
+
+class RenewBookFormTest(TestCase):
+    def test_renew_form_date_field_label(self):
+        form = SignUpForm(data={'username': 'foo',
+                                'email': 'alice@example.com',
+                                'password1': 'foo',
+                                'password2': 'foo'})
+        self.failIf(form.is_valid())
+        self.assertEqual(form.errors['email'],
+                         [u"This email address is already in use. Please supply a different email address."])
+
+        form = forms.SignUpForm(data={'username': 'foo',
+                                      'email': 'foo@example.com',
+                                      'password1': 'foo',
+                                      'password2': 'foo'})
+        self.failUnless(form.is_valid())
+
+    def test_validates_password(self):
+        user = User.objects.create_user(username='testclient', password='sekret')
+        data = {
+            'old_password': 'sekret',
+            'new_password1': 'testclient',
+            'new_password2': 'testclient',
+        }
+        form = PasswordChangeForm(user, data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form["new_password2"].errors), 1)
+
