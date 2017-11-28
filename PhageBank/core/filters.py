9e157e22cfe94c9955850b6faf8984ce21f9bf9f
thesaurabhkumar <thesaurabh@tamu.edu>
2017-11-27 19:32:57 -0600

    Adding dropdowns to select Years/Months in Advanced Search

--- PhageBank/core/filters.py
+++ PhageBank/core/filters.py
@@ -8,16 +8,39 @@ from PhageBank.core.models import PhageData, ExperimentData, IsolationData
 
 
 class PhageFilter(django_filters.FilterSet):
+    MONTHS = (
+        ('1', 'January'),
+        ('2', 'February'),
+        ('3', 'March'),
+        ('4', 'April'),
+        ('5', 'May'),
+        ('6', 'June'),
+        ('7', 'July'),
+        ('8', 'August'),
+        ('9', 'September'),
+        ('10', 'October'),
+        ('11', 'November'),
+        ('12', 'December'),
+    )
+    YEARS = []
+    for i in range(1990, 2020):
+        yentry = []
+        yentry.append(str(i))
+        yentry.append(str(i))
+        YEARS.append(yentry)
+    YEARS = tuple(YEARS)
+
     phage_name = django_filters.CharFilter(lookup_expr='icontains')
     phage_host_name = django_filters.CharFilter(lookup_expr='icontains')
     phage_isolator_name = django_filters.CharFilter(lookup_expr='icontains')
-    submitted_year_gt = django_filters.NumberFilter(label='Year Submitted After', name='phage_submitted_date', lookup_expr='year__gte')
-    submitted_month_gt = django_filters.NumberFilter(label='Month Submitted After', name='phage_submitted_date', lookup_expr='month__gte')
-    submitted_year_lt = django_filters.NumberFilter(label='Year Submitted Before', name='phage_submitted_date', lookup_expr='year__lte')
-    submitted_month_lt = django_filters.NumberFilter(label='Month Submitted Before', name='phage_submitted_date', lookup_expr='month__lte')
-    #submitted_month_lt = django_filters.NumberFilter(label='Month Submitted Before', name='phage_submitted_date',lookup_expr='month__lt')
-    #date_range_gt = django_filters.DateFilter(label='Date Submitted After', name='phage_submitted_date', lookup_expr='lt',
-    #                                                  widget=forms.SelectDateWidget(attrs={'placeholder': 'YYYY/MM/DD'}))
+    #submitted_year_gt = django_filters.NumberFilter(label='Year Submitted After', name='phage_submitted_date', lookup_expr='year__gte')
+    submitted_year_gt = django_filters.ChoiceFilter(label='Year Submitted After', name='phage_submitted_date', choices=YEARS, lookup_expr='year__gte')
+    #submitted_month_gt = django_filters.NumberFilter(label='Month Submitted After', name='phage_submitted_date', lookup_expr='month__gte')
+    submitted_month_gt = django_filters.ChoiceFilter(label='Month Submitted After', name='phage_submitted_date', choices=MONTHS, lookup_expr='month__gte')
+    #submitted_year_lt = django_filters.NumberFilter(label='Year Submitted Before', name='phage_submitted_date', lookup_expr='year__lte')
+    submitted_year_lt = django_filters.ChoiceFilter(label='Year Submitted Before', name='phage_submitted_date', choices=YEARS, lookup_expr='year__lte')
+    #submitted_month_lt = django_filters.NumberFilter(label='Month Submitted Before', name='phage_submitted_date', lookup_expr='month__lte')
+    submitted_month_lt = django_filters.ChoiceFilter(label='Month Submitted Before', name='phage_submitted_date', choices=MONTHS, lookup_expr='month__lte')
 
     INCIDENT_LIVE = (
         ('0', 'Lab-A'),
