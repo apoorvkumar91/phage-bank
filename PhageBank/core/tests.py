5f38bf9c3a49e9c70d529afad6543110a78ce7e6
Apoorv Kumar <apoorvkumar91@tamu.edu>
2017-11-28 01:26:05 -0600

    Fixed previous test cases

--- PhageBank/core/tests.py
+++ PhageBank/core/tests.py
@@ -3,42 +3,79 @@ import unittest
 from django.test import Client
 from django.contrib.auth.models import User
 
-from faker import Faker
-fake=Faker()
-
-username=fake.word()
-
-class SimpleTest(unittest.TestCase):
-    user = User.objects.create(username=username, password='pass@123', email='testuser@test.com')
-    user.save()
+class SimpleTest(TestCase):    #unittest.TestCase
     valid_credentials = {
-        'username': username,
+        'username': "test_user",
         'password': 'pass@123'}
     invalid_credentials = {
         'username': 'abcde',
         'password': '12345'}
-    client = Client()
+
+    #client = Client()
 
     def setUp(self):
         # Every test needs a client.
-        self.client = Client()
-        
+        user = User.objects.create_user("test_user", 'testuser@test.com', 'pass@123')
+        user.save()
+
     def test_details(self):
         # Issue a GET request.
-        response = self.client.get('/login/')
+        response = self.client.get('/mylogin/')
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
     def test_user_exist(self):
         # Check that login is successful with valid user
-        response = self.client.post('/login/', self.valid_credentials, follow=True)
-        self.assertEqual(response.status_code,200)
+        response = self.client.post('/mylogin/', self.valid_credentials, follow=True)
+        #self.assertEqual(response.status_code,200)
+        
+        form_is_valid = response.json()['form_is_valid']
+        self.assertEqual(form_is_valid,True)
 
     def test_user_doesnt_exist(self):
         # Check that login is unsuccessful with invalid user
-        response = self.client.post('/login/', self.invalid_credentials, follow=True)
-        index = response.content.find(b"Your username and password didn\'t match. Please try again.")
+        response = self.client.post('/mylogin/', self.invalid_credentials, follow=True)
+        #index = response.content.find(b"Your username and password didn\'t match. Please try again.")
         
-        self.assertNotEqual(index,-1)
+        form_is_valid = response.json()['form_is_valid']
+        
+        self.assertEqual(form_is_valid,False)
 # Create your tests here.
+
+#    def add_phage_ut(self):
+#        phage_desc = {"phage_name" : "test_newphage", "phage_CPT_id" : "test_123"}
+#        response = self.client.post('/add_phage/', phage_desc, follow=True)
+#        approvePhage = response.json()['approvePhage']
+#        approveCPTid = response.json()['approveCPTid']
+#        
+#        self.assertEqual(approvePhage,1)
+#        self.assertEqual(approveCPTid,1)
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
